11509c4de91f185ec20eae083c4ed269bd2569b8
matveii <matveiinartymov@gmail.com>
2025-11-19 15:55:47 +0300

    small fixes

--- src/core/bot/on_message.py
+++
@@ -1,9 +0,0 @@
-from bot import bot
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-    
-    if message.content.lower() == 'ping':
-        await message.channel.send('pong')
